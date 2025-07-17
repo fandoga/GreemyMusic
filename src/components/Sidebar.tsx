0ffc3082f0aa02c43fb74002db235571f243940c
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-07-17 19:21:15 +0300

    S

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
     const loading = useLoading();
 
     useEffect(() => {
-        const accessToken = localStorage.getItem('spotify-access-token');
+        const accessToken = localStorage.getItem('access-token');
         console.log('accessToken:', accessToken);
         if (!accessToken) return;
 
