9a2dcc6b1db574afec11ccf260c16442e118d9ca
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-07-21 15:15:49 +0300

    recommendations set

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -16,8 +16,8 @@ const Main = () => {
             const accessToken = localStorage.getItem('access-token');
             setLoading(true)
             const res = await fetch(
-                // `https://api.spotify.com/v1/playlists/1vCsCr74SWGrwv3RMNcBPg/tracks?limit=20`,
-                `https://api.spotify.com/v1/playlists/37i9dQZF1E366gZNq84Mhw/tracks?limit=20`,
+                `https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF/tracks?limit=20`,
+                // `https://api.spotify.com/v1/playlists/37i9dQZF1E366gZNq84Mhw/tracks?limit=20`,
                 {
                     headers: { Authorization: `Bearer ${accessToken}` },
                 }
