ee4a8613024bc95b0c6919e54df6989c6f33d116
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-07-21 16:18:39 +0300

    recommendations set

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -22,29 +22,33 @@ const Nav = () => {
 
     return (
         <nav className="main__nav nav">
-            <div className="nav__logo logo">
-                <Link to={'/'}>
-                    <img className="logo__image" src="/img/logo.png" alt="logo" />
-                </Link>
+            <div className="nav__top">
+                <div className="nav__logo logo">
+                    <Link to={'/'}>
+                        <img className="logo__image" src="/img/logo.png" alt="logo" />
+                    </Link>
+                </div>
+                <div onClick={toggleMenu} className="nav__burger burger">
+                    <span className="burger__line"></span>
+                    <span className="burger__line"></span>
+                    <span className="burger__line"></span>
+                </div>
+                <div className="nav__menu menu"
+                    style={{ opacity: menuOpen ? "1" : "0", display: displayMenu ? "block" : "none" }}>
+                    <ul className="menu__list">
+                        <li className="menu__item">
+                            <Link to="/" className="menu__link">Главная</Link>
+                        </li>
+                        <li className="menu__item">
+                            <Link to="/playlist" className="menu__link">Мой плейлист</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div onClick={toggleMenu} className="nav__burger burger">
-                <span className="burger__line"></span>
-                <span className="burger__line"></span>
-                <span className="burger__line"></span>
-            </div>
-            <div className="nav__menu menu"
-                style={{ opacity: menuOpen ? "1" : "0", display: displayMenu ? "block" : "none" }}>
-                <ul className="menu__list">
-                    <li className="menu__item">
-                        <Link to="/" className="menu__link">Главная</Link>
-                    </li>
-                    <li className="menu__item">
-                        <Link to="/playlist" className="menu__link">Мой плейлист</Link>
-                    </li>
-                    <li className="menu__item">
-                        <Link to="/login" className="menu__link">Выйти</Link>
-                    </li>
-                </ul>
+            <div className="nav__footer">
+                <div className="nav__sing-out">
+                    <Link to="/login" className="menu__link">Выйти</Link>
+                </div>
             </div>
         </nav>
     );
