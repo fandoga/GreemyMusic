97821811839d9ccdb4dc24a66fcb1421ead8da38
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-07-21 14:56:23 +0300

    recommendations set

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -17,7 +17,7 @@ const Main = () => {
             setLoading(true)
             const res = await fetch(
                 // `https://api.spotify.com/v1/playlists/1vCsCr74SWGrwv3RMNcBPg/tracks?limit=20`,
-                'https://api.spotify.com/v1/recommendations?seed_tracks=7gLAsZ9ZLXPzwhXefnvKcg',
+                'https://api.spotify.com/v1/recommendations?seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA',
                 {
                     headers: { Authorization: `Bearer ${accessToken}` },
                 }
