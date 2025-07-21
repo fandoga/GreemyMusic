781034a59a379e146c53deff09cabccd4022f4f6
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-07-21 14:39:28 +0300

    ip check

--- src/pages/IpCheck/NeedVpn.tsx
+++ src/pages/IpCheck/NeedVpn.tsx
@@ -1,5 +1,6 @@
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import './needvpn.css'
 
 const NeedVpn = () => {
 
