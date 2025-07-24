b278084b9711dc8e83f75953e6a44fb0b2d3e9c8
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-07-24 17:20:33 +0300

    making player

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -50,7 +50,7 @@ const Bar: React.FC<BarProps> = ({ state, track }) => {
                                 </div>
                                 <div onClick={togglePlay} className="player__btn-play _btn">
                                     <svg className="player__btn-play-svg" >
-                                        <use xlinkHref={isPlaying ? '/img/icon/pause.svg' : '/img/icon/sprite.svg#icon-play'}></use>
+                                        <use xlinkHref={isPlaying ? '/img/icon/pause.svg' : '/img/icon/sprite.svg#icon-play'} width="22" height="20"></use>
                                     </svg>
                                 </div>
                                 <div className="player__btn-next">
