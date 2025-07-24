2df14452f04537fa1af5041c6b11dad2d7b5b6c9
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-07-24 16:47:42 +0300

    adding playlists

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import BarSkeleton from "./BarSkeleton"
 import { useLoading } from "../context/LoadingContext";
 import TrackData from "../pages/main/TrackData";
@@ -11,6 +11,11 @@ interface BarProps {
 
 const Bar: React.FC<BarProps> = ({ state, track }) => {
     const loading = useLoading();
+    const audioRef = useRef<any>(null);
+
+    const PlayHandle = () => {
+        audioRef.current.play()
+    }
 
     if (loading || state) {
         return (
@@ -18,95 +23,98 @@ const Bar: React.FC<BarProps> = ({ state, track }) => {
         )
     }
 
-
-
-
     return (
-        <div className="bar">
-            <div className="bar__content">
-                <div className="bar__player-progress"></div>
-                <div className="bar__player-block">
-                    <div className="bar__player player">
-                        <div className="player__controls">
-                            <div className="player__btn-prev">
-                                <svg className="player__btn-prev-svg" >
-                                    <use xlinkHref="/img/icon/sprite.svg#icon-prev"></use>
-                                </svg>
-                            </div>
-                            <div className="player__btn-play _btn">
-                                <svg className="player__btn-play-svg" >
-                                    <use xlinkHref="/img/icon/sprite.svg#icon-play"></use>
-                                </svg>
-                            </div>
-                            <div className="player__btn-next">
-                                <svg className="player__btn-next-svg" >
-                                    <use xlinkHref="/img/icon/sprite.svg#icon-next"></use>
-                                </svg>
-                            </div>
-                            <div className="player__btn-repeat _btn-icon">
-                                <svg className="player__btn-repeat-svg">
-                                    <use xlinkHref="/img/icon/sprite.svg#icon-repeat"></use>
-                                </svg>
-                            </div>
-                            <div className="player__btn-shuffle _btn-icon">
-                                <svg className="player__btn-shuffle-svg" >
-                                    <use xlinkHref="/img/icon/sprite.svg#icon-shuffle"></use>
-                                </svg>
-                            </div>
-                        </div>
+        <>
+            <audio controls ref={audioRef}>
+                <source src="/public/audio/BLACKPINK.mp3" type="audio/mp3" />
+            </audio>
 
-                        <div className="player__track-play track-play">
-                            <div className="track-play__contain">
-                                <div className="track-play__image">
-                                    <img src={track?.ImgMed} alt="" />
-                                </div>
-                                <div className="track-play__author">
-                                    <a className="track-play__author-link" href="http://"
-                                    >{track?.Name}</a>
-                                </div>
-                                <div className="track-play__album">
-                                    <a className="track-play__album-link" href="http://">{
-                                        track?.Author && track.Author.length > 28
-                                            ? track.Author.slice(0, 28) + "..."
-                                            : track?.Author || ""}</a>
-                                </div>
-                            </div>
-
-                            <div className="track-play__like-dis">
-                                <div className="track-play__like _btn-icon">
-                                    <svg className="track-play__like-svg">
-                                        <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+            <div className="bar">
+                <div className="bar__content">
+                    <div className="bar__player-progress"></div>
+                    <div className="bar__player-block">
+                        <div className="bar__player player">
+                            <div className="player__controls">
+                                <div className="player__btn-prev">
+                                    <svg className="player__btn-prev-svg" >
+                                        <use xlinkHref="/img/icon/sprite.svg#icon-prev"></use>
                                     </svg>
                                 </div>
-                                <div className="track-play__dislike _btn-icon">
-                                    <svg className="track-play__dislike-svg" >
-                                        <use
-                                            xlinkHref="/img/icon/sprite.svg#icon-dislike"
-                                        ></use>
+                                <div onClick={PlayHandle} className="player__btn-play _btn">
+                                    <svg className="player__btn-play-svg" >
+                                        <use xlinkHref="/img/icon/sprite.svg#icon-play"></use>
+                                    </svg>
+                                </div>
+                                <div className="player__btn-next">
+                                    <svg className="player__btn-next-svg" >
+                                        <use xlinkHref="/img/icon/sprite.svg#icon-next"></use>
+                                    </svg>
+                                </div>
+                                <div className="player__btn-repeat _btn-icon">
+                                    <svg className="player__btn-repeat-svg">
+                                        <use xlinkHref="/img/icon/sprite.svg#icon-repeat"></use>
+                                    </svg>
+                                </div>
+                                <div className="player__btn-shuffle _btn-icon">
+                                    <svg className="player__btn-shuffle-svg" >
+                                        <use xlinkHref="/img/icon/sprite.svg#icon-shuffle"></use>
                                     </svg>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="bar__volume-block volume">
-                        <div className="volume__content">
-                            <div className="volume__image">
-                                <svg className="volume__svg">
-                                    <use xlinkHref="/img/icon/sprite.svg#icon-volume"></use>
-                                </svg>
+
+                            <div className="player__track-play track-play">
+                                <div className="track-play__contain">
+                                    <div className="track-play__image">
+                                        <img src={track?.ImgMed} alt="" />
+                                    </div>
+                                    <div className="track-play__author">
+                                        <a className="track-play__author-link" href="http://"
+                                        >{track?.Name}</a>
+                                    </div>
+                                    <div className="track-play__album">
+                                        <a className="track-play__album-link" href="http://">{
+                                            track?.Author && track.Author.length > 28
+                                                ? track.Author.slice(0, 28) + "..."
+                                                : track?.Author || ""}</a>
+                                    </div>
+                                </div>
+
+                                <div className="track-play__like-dis">
+                                    <div className="track-play__like _btn-icon">
+                                        <svg className="track-play__like-svg">
+                                            <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+                                        </svg>
+                                    </div>
+                                    <div className="track-play__dislike _btn-icon">
+                                        <svg className="track-play__dislike-svg" >
+                                            <use
+                                                xlinkHref="/img/icon/sprite.svg#icon-dislike"
+                                            ></use>
+                                        </svg>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="volume__progress _btn">
-                                <input
-                                    className="volume__progress-line _btn"
-                                    type="range"
-                                    name="range"
-                                />
+                        </div>
+                        <div className="bar__volume-block volume">
+                            <div className="volume__content">
+                                <div className="volume__image">
+                                    <svg className="volume__svg">
+                                        <use xlinkHref="/img/icon/sprite.svg#icon-volume"></use>
+                                    </svg>
+                                </div>
+                                <div className="volume__progress _btn">
+                                    <input
+                                        className="volume__progress-line _btn"
+                                        type="range"
+                                        name="range"
+                                    />
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
