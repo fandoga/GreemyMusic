a6304a898492f426ed6ab592bc79be8a01539e46
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-07-25 14:17:52 +0300

    making player

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -5,18 +5,18 @@ const ProgressBar = () => {
     const duration = 230;
 
     return (
-        <div className="progress__bar">
-            <input
-                className="bar__player-progress"
-                type="range"
-                min={0}
-                max={duration}
-                value={currentTime}
-                step={0.01}
-                onChange={(e: any) => setCurrentTime(e.target.value)}
-                color="#ff0000"
-            />
-        </div>
+
+        <input
+            className="bar__player-progress"
+            type="range"
+            min={0}
+            max={duration}
+            value={currentTime}
+            step={0.01}
+            onChange={(e: any) => setCurrentTime(e.target.value)}
+            color="#ff0000"
+        />
+
     );
 }
 
