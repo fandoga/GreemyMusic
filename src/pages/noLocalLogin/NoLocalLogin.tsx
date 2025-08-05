6facce4f9d8d0cfa55e31dd6fa071b53b9772a68
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-08-05 18:26:00 +0300

    some fixes

--- src/pages/noLocalLogin/NoLocalLogin.tsx
+++ src/pages/noLocalLogin/NoLocalLogin.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../404/notfound.css'
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NoLocalLogin = () => {
     const navigate = useNavigate()
@@ -11,7 +11,7 @@ const NoLocalLogin = () => {
         <div className="main__notfound">
             <h1 className="notfound__title">Упс</h1>
             <h2 className="notfound__subtitle">К сожалению локальная регистрация пока не доступна😭<br>Пожалуйста используйте аккаунт Spotify</br></h2>
-            <p className="notfound__text">Она обязательно появиться в будущем, разработчик пока не успел:/
+            <p className="notfound__text">Она обязательно появиться в будущем, разработчик пока не успел
             </p>
             <button onClick={() => navigate('/login')} className="notfound__button">Войти со Spotify</button>
         </div>
