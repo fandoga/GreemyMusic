4b126de6a61fc390dba07c8b85cb7c74879ac0df
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-08-05 15:01:06 +0300

    some fixes

--- src/components/NavSkeleton.tsx
+++ src/components/NavSkeleton.tsx
@@ -1,17 +1,20 @@
 const NavSkeleton = () => {
     return (
-        <div className="nav__menu menu">
-            <ul className="menu__list">
-                <li className="menu__item">
-                    <p className="skeleton__text"></p>
-                </li>
-            </ul>
-            <ul className="playlist__list">
-                <li className="playlist-list__item">
-                    <p className="skeleton__text"></p>
-                </li>
-            </ul>
-        </div>
+
+        <ul className="playlist__list">
+            <li className="playlist-list__item">
+                <p className="skeleton__text"></p>
+            </li>
+            <li className="playlist-list__item">
+                <p className="skeleton__text"></p>
+            </li>
+            <li className="playlist-list__item">
+                <p className="skeleton__text"></p>
+            </li>
+            <li className="playlist-list__item">
+                <p className="skeleton__text"></p>
+            </li>
+        </ul>
     );
 }
 
