3638e62d7911af15f19d805b185bea31991533a7
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-08-05 19:10:22 +0300

    some fixes

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -2,40 +2,25 @@ import React, { ReactElement, useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import NeedVpnPage from '../pages/IpCheck/NeedVpnPage';
 
+
+
 const isAuthenticated = () => {
     return !!localStorage.getItem('access-token');
 };
 
 const PrivateRoute = ({ children }: { children: ReactElement }) => {
-    const [vpn, setVpn] = useState<boolean | null>(null); // null = проверка ещё идёт
+    const [vpn, setVpn] = useState(false);
 
     useEffect(() => {
-        const checkVpn = async () => {
-            try {
-                const res = await fetch('https://ipinfo.io/json?token=663adf1cf972e9');
-                const data = await res.json();
+        fetch('https://ipinfo.io/json?token=663adf1cf972e9')
+            .then(res => res.json())
+            .then(data => {
                 const country = data.country;
-                setVpn(country !== "RU");
-            } catch (e) {
-                // В случае ошибки считаем, что VPN неактивен (или выводим другую страницу)
-                setVpn(false);
-            }
-        };
+                country === "RU" ? setVpn(false) : setVpn(true);
+            });
+    }, [])
 
-        checkVpn();
-    }, []);
-
-    if (vpn === null) {
-        // Пока проверка идёт — можно вернуть null или прелоадер
-        return <div>Проверка VPN...</div>;
-    }
-
-    // После проверки
-    if (!isAuthenticated()) {
-        return <Navigate to="/login" replace />;
-    }
-
-    return vpn ? children : <NeedVpnPage />;
+    return isAuthenticated() ? (vpn ? children : <NeedVpnPage />) : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
