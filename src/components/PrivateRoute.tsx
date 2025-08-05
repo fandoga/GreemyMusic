ee40e2ff11940efd5f58fa3f35288c200403de5a
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-08-05 18:55:18 +0300

    some fixes

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,12 +1,24 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
+import NeedVpn from '../pages/IpCheck/NeedVpn';
+
+
 
 const isAuthenticated = () => {
     return !!localStorage.getItem('access-token');
 };
 
 const PrivateRoute = ({ children }: { children: ReactElement }) => {
-    return isAuthenticated() ? children : <Navigate to="/login" replace />;
+    const [vpn, setVpn] = useState(false);
+
+    fetch('https://ipinfo.io/json?token=663adf1cf972e9')
+        .then(res => res.json())
+        .then(data => {
+            const country = data.country;
+            country === "RU" ? setVpn(false) : setVpn(true);
+        });
+
+    return isAuthenticated() ? (vpn ? children : <NeedVpn />) : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
