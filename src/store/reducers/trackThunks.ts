924cb5302f89df441efd78a8c49f0539662d105f
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-09-25 12:48:08 +0300

    reworking on redux

--- src/store/reducers/trackThunks.ts
+++ src/store/reducers/trackThunks.ts
@@ -16,7 +16,7 @@ export const fetchRecomendations = createAsyncThunk<
         const accessToken = localStorage.getItem('access-token');
         try {
             const res = await fetch(
-                `https://api.spotify.com/v1/playlists/3xMQTDLOIGvj3lWH5e5x6F/tracks?limit=${limit}`,
+                `https://api.spotify.com/v1/playlists/3xMQTDLOIGvj3lWH5e5x6F/tracks?limit=${limit}&offset=${offset}`,
                 { headers: { Authorization: `Bearer ${accessToken}` } }
             );
             const data = await res.json();
