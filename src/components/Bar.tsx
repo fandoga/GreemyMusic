8431bfabed4aac908f482a21cf3baf8e9346c996
Artem / fandy <artem.nesterenko.1984@bk.ru>
2025-10-01 19:34:47 +0300

    reworking on redux

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -45,7 +45,7 @@ const Bar: React.FC<BarProps> = ({ state }) => {
         if (!audioRef.current) return;
         dispatch(stopPlayingTrack())
         audioRef.current.currentTime = 0
-        setCurrentTime(0)
+        // setCurrentTime(audioRef.current.currentTime)
     }, [currentTrack])
 
     useEffect(() => {
